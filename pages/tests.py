46aef469ae7c23027dcc0a15198517532889c8f4
Cowboy #1 <ed@debian-BULLSEYE-live-builder-AMD64>
2022-01-31 22:41:05 +0100

    Fix typo

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,4 @@
-from test import SimpleTestCase
+from django.test import SimpleTestCase
 
 class SimpleTests(SimpleTestCase): 
     def test_home_page_status_code(self):
@@ -6,6 +6,6 @@ class SimpleTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_about_page_status_code(self):
-        response = self.client.get('about/')
+        response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
 
